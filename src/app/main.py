78d6e11bd56e05dcfd6f9c375b7c2b8bab206032
khoidd <khoidd@outlook.com>
2021-10-07 20:27:06 +0700

    init proj

---
+++ src/app/main.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+host: str = "0.0.0.0"
+port: int = 8081
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host=host, port=port, workers=10)
